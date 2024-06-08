d2d3a08e890add29e724bf3503d352cf1f84fbc2
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2024-06-08 01:28:38 -0300

    Adapt to rgblight changes

--- users/rhruiz/rgblight/rgblight.c
+++ users/rhruiz/rgblight/rgblight.c
@@ -10,26 +10,7 @@ void rhruiz_rgblight_reset(void) {
 void rhruiz_change_leds_to(uint16_t hue, uint8_t sat) {
     rgblight_config_t eeprom_config;
     eeprom_config.raw = eeconfig_read_rgblight();
-    rgb_led_t *ledp;
 
-#ifdef RGBLIGHT_LED_MAP
-    rgb_led_t led0[RGBLIGHT_LED_COUNT];
-    for (uint8_t i = 0; i < RGBLIGHT_LED_COUNT; i++) {
-        led0[i] = led[pgm_read_byte(&led_map[i])];
-    }
-    ledp = led0;
-#else
-    ledp = led;
-#endif
-
-    for (uint8_t i = RGBLIGHT_LED_COUNT; i-- > 0;) {
-        sethsv(hue, sat, eeprom_config.val, (rgb_led_t *)&ledp[i]);
-    }
-
-#ifdef RGBW
-    ws2812_setleds_rgbw(ledp, RGBLIGHT_LED_COUNT);
-#else
-    ws2812_setleds(ledp, RGBLIGHT_LED_COUNT);
-#endif
+    rgblight_sethsv_range(hue, sat, eeprom_config.val, 0, RGBLIGHT_LED_COUNT - 1);
 }
 #endif
