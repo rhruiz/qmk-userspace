d01711000378b0713fc0a16ec2a19d5c0f7822c6
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2025-05-26 19:02:44 -0300

    eeconfig_read_rgblight changed signature

--- users/rhruiz/rgblight/rgblight.c
+++ users/rhruiz/rgblight/rgblight.c
@@ -9,7 +9,7 @@ void rhruiz_rgblight_reset(void) {
 
 void rhruiz_change_leds_to(uint16_t hue, uint8_t sat) {
     rgblight_config_t eeprom_config;
-    eeprom_config.raw = eeconfig_read_rgblight();
+    eeconfig_read_rgblight(&eeprom_config);
 
     rgblight_sethsv_range(hue, sat, eeprom_config.val, 0, RGBLIGHT_LED_COUNT - 1);
 }
