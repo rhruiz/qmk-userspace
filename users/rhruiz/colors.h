ec8935e5b0040003a6ce44b6e0aaeaea348fc427
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2025-09-03 17:57:11 -0300

    tweak colors, sync hsv to rgb

--- users/rhruiz/colors.h
+++ users/rhruiz/colors.h
@@ -4,7 +4,7 @@
 #define LWR_RGB 0x0f, 0xff, 0xfb
 
 #define RSE_HSV 7, 255, 255
-#define RSE_RGB 0xff, 0x2a, 0x00
+#define RSE_RGB 0xff, 0x80, 0x00
 
 #define AUG_HSV 85, 255, 255
 #define AUG_RGB RGB_GREEN
@@ -12,13 +12,13 @@
 #define CFG_HSV 201, 255, 255
 #define CFG_RGB 0xb7, 0x00, 0xff
 
-#define FUN_HSV 0, 255, 255
+#define FUN_HSV 148, 255, 255
 #define FUN_RGB 0x47, 0xa7, 0xed
 
 #define NUM_HSV 0, 255, 255
 #define NUN_RGB 140, 40, 0
 
-#define CAPS_HSV 201, 255, 255
+#define CAPS_HSV 180, 255, 255
 #define CAPS_RGB 64, 0, 0
 
 #define MAC_HSV 0, 0, 255
